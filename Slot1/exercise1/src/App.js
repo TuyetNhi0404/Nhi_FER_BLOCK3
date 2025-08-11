4c460caa09a1eb647dfe39a74ef35296eaecd30b
TuyetNhi0404 <tuyetnhi19044.2004@gmail.com>
2025-08-11 10:38:00 +0700

    Fix exercise1 folder - remove nested git repository

---
+++ Slot1/exercise1/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const myname = "Bui Thi Tuyet Nhi"; // Define the variable
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello React</h1>
+        <p>
+          My name is {myname} and I am learning React!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
