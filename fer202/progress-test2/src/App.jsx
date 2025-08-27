d44ac4c38f5c6dd2e90d77f2ee2b7afa54a94c0f
TuyetNhi0404 <tuyetnhi19044.2004@gmail.com>
2025-08-27 12:47:59 +0700

    fix route

--- fer202/progress-test2/src/App.jsx
+++ fer202/progress-test2/src/App.jsx
@@ -5,9 +5,9 @@ function App() {
   
   return (
     <>
-      <div className='container py-4'>
+     
         <AppRoutes />
-      </div>
+      
     </>
   )
 }
--- fer202/progress-test2/src/routes/AppRoutes.jsx
+++ fer202/progress-test2/src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import ProductsPage from '../pages/ProductsPage';
 import ProductDetailsPage from '../pages/ProductDetails';
+import LoginPage from '../pages/LoginPage'; // import LoginPage
 
 export default function AppRoutes() {
   return (
@@ -8,6 +9,7 @@ export default function AppRoutes() {
       <Route path="/" element={<Navigate to="/products" replace />} />
       <Route path="/products" element={<ProductsPage />} />
       <Route path="/products/:id" element={<ProductDetailsPage />} />
+      <Route path="/login" element={<LoginPage />} />   {/* thêm dòng này */}
       <Route path="*" element={<Navigate to="/products" replace />} />
     </Routes>
   );
